198e3eb73624640904f607495616a7f8cb177bfb
Aaron Ten Clay <aaron@techtonium.com>
2011-01-16 12:07:46 -0800

    Initial code

---
+++ class.AccountManager.php
@@ -0,0 +1,24 @@
+<?php
+
+	/**
+	 * \brief Main interface to account management
+	 */
+	 class AccountManager {
+
+
+		/**
+		 * \brief Constructs an AccountManager object
+		 */
+		function __construct() {
+
+
+
+			// Check to see all authentication providers are registered in the database
+			$this->CheckAndRegisterProviders();
+
+		}
+
+
+	 }
+
+?>
